27663bd9efa496432dbbb5ddc818963d57d00557
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-02-15 09:41:06 +0100

    getting started

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 let package = Package(
     name: "ekg-swift",
     dependencies: [
-        .Package(url: "https://github.com/kylef/Curassow.git", majorVersion: 0, minor: 3)
+        .Package(url: "https://github.com/kylef/Curassow.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/nestproject/Frank.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/czechboy0/Redbird.git", majorVersion: 0)
+        // .Package(url: "https://github.com/apple/swift-corelibs-xctest.git", majorVersion: 0)
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,6 +1,23 @@
 import Inquiline
 import Curassow
+import Frank
+import Redbird
 
-serve { _ in
-    return Response(.Ok, contentType: "text/plain", body: "Hello World")
+do {
+
+	let redis = try Redbird(port: 6379)
+
+	get { request in
+	  	return Response(.Ok, contentType: "text/plain", body: "Pong")
+	}
+
+	get("v1") { request in
+		return Response(.Ok, contentType: "text/plain", body: "Did things")
+	}
+
+	// start the server
+	serve(call)
+} catch {
+	fatalError("\(error)")
 }
+

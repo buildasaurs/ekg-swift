d8052175d761d1f5fc817bee180f81c524c3c1b1
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-02-25 01:56:07 +0000

    added validation

--- Sources/EventValidation.swift
+++ Sources/EventValidation.swift
@@ -1,4 +1,11 @@
 
 func validateEvent(event: [String: Any]) -> Bool {
+
+	guard let app = event["app"] else { return false }
+	guard let token = event["token"] else { return false }
+	guard let eventType = event["event_type"] else { return false }
+	guard let version = event["version"] else { return false }
+	guard let build = event["build"] else { return false }
+
 	return true
 }

58366a9e287bdb8938b7fe014a72b91309881715
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-04-22 09:46:05 +0200

    switched to official 0.6 vapor

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "ekg-swift",
     dependencies: [
-        .Package(url: "https://github.com/LoganWright/vapor.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 6),
         .Package(url: "https://github.com/czechboy0/Redbird.git", majorVersion: 0),
         .Package(url: "https://github.com/czechboy0/Environment.git", majorVersion: 0),
         .Package(url: "https://github.com/czechboy0/Jay.git", majorVersion: 0)
